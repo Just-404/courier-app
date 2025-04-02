3ad3af4bc2c93c3ed8b9fe64a86a9ac16b2ff2ae
Eddy <154628904+Just-404@users.noreply.github.com>
2025-04-01 22:02:38 -0400

    Add icons to sidebar

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Icons attributtion:
 1. User icon created by [Freepik](https://www.flaticon.com/free-icons/user) - [Flaticon](https://www.flaticon.com/)
 2. Password icon created by [Pixel perfect](https://www.flaticon.com/free-icons/password) - [Flaticon](https://www.flaticon.com/)
 3. Role icon created by [Freepik](https://www.flaticon.com/free-icons/roles) - [Flaticon](https://www.flaticon.com/)
+4. Dashboard icon created by Pixel perfect - Flaticon
+5. Order icon created by vectorsmarket15 - Flaticon
+6. Courier icon created by zero_wing - Flaticon
+7. T-shirt icon created by edt.im - Flaticon
+8. Refresh icon created by Dave Gandy - Flaticon
+9. History icon created by sonnycandra - Flaticon
+10. Order tracking icon created by edt.im - Flaticon
+11. Received icon created by kerismaker - Flaticon
--- pacas-app-ui/src/utils/UserSection.js
+++ pacas-app-ui/src/utils/UserSection.js
@@ -1,27 +1,35 @@
-import userIcon from "../assets/user.png"; // icon template for now
+import userIcon from "../assets/user.png";
+import dashboardIcon from "../assets/dashboard.png";
+import orderManagementIcon from "../assets/Orders/order-delivery.png";
+import pacaManagementIcon from "../assets/Pacas/t-shirt.png";
+import ordersReceivedIcon from "../assets/Orders/received.png";
+import historyIcon from "../assets/Orders/history.png";
+import pendingOrderIcon from "../assets/Pacas/booking.png";
+import orderTrackingIcon from "../assets/Orders/order-tracking.png";
+import updateStatus from "../assets/Orders/refresh-page-option.png";
 
 const adminSection = [
-  { id: 1, title: "Dashboard", icon: userIcon },
+  { id: 1, title: "Dashboard", icon: dashboardIcon },
   { id: 2, title: "Users Management", icon: userIcon },
-  { id: 3, title: "Orders Management", icon: userIcon },
-  { id: 4, title: "Pacas Management", icon: userIcon },
+  { id: 3, title: "Orders Management", icon: orderManagementIcon },
+  { id: 4, title: "Pacas Management", icon: pacaManagementIcon },
 ];
 
 const providerSection = [
-  { id: 1, title: "Pacas Management", icon: userIcon },
-  { id: 2, title: "Orders Received", icon: userIcon },
-  { id: 3, title: "Sales History", icon: userIcon },
+  { id: 1, title: "Pacas Management", icon: pacaManagementIcon },
+  { id: 2, title: "Orders Received", icon: ordersReceivedIcon },
+  { id: 3, title: "Sales History", icon: history },
 ];
 
 const transporterSection = [
-  { id: 1, title: "Pending Orders", icon: userIcon },
-  { id: 2, title: "Update Delivery Status", icon: userIcon },
-  { id: 3, title: "Orders History", icon: userIcon },
+  { id: 1, title: "Pending Orders", icon: pendingOrderIcon },
+  { id: 2, title: "Update Track Status", icon: updateStatus },
+  { id: 3, title: "Orders History", icon: historyIcon },
 ];
 const distributorSection = [
-  { id: 1, title: "Order Pacas", icon: userIcon },
-  { id: 2, title: "Order Tracking", icon: userIcon },
-  { id: 3, title: "Orders History", icon: userIcon },
+  { id: 1, title: "Order Pacas", icon: pacaManagementIcon },
+  { id: 2, title: "Order Tracking", icon: orderTrackingIcon },
+  { id: 3, title: "Orders History", icon: historyIcon },
 ];
 export default {
   adminSection,
